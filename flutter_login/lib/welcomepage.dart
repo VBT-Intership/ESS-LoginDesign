7495f3f78cc69ed6afb2f4b60af6095cda16fd96
Ebubekir <32433476+EbuSezer@users.noreply.github.com>
2020-07-13 23:40:17 +0300

    Welcome page titles added

--- flutter_login/lib/welcomepage.dart
+++ flutter_login/lib/welcomepage.dart
@@ -4,7 +4,49 @@ class WelcomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("Hello"),
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SignInSuccessLabel(),
+          SizedBox(height: 15),
+          WelcomeLabel(),
+        ],
+      ),
+    );
+  }
+}
+
+class WelcomeLabel extends StatelessWidget {
+  const WelcomeLabel({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "Welcome to Hysys",
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 28.0,
+      ),
+    );
+  }
+}
+
+class SignInSuccessLabel extends StatelessWidget {
+  const SignInSuccessLabel({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "Sign in successful!",
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 14.0,
+      ),
     );
   }
 }
