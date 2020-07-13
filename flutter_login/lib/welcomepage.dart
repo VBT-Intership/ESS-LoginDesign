3f628a1d078ffd37e969e61039047d941c290078
Ebubekir <32433476+EbuSezer@users.noreply.github.com>
2020-07-13 23:47:29 +0300

    Circle Done extracted

--- flutter_login/lib/welcomepage.dart
+++ flutter_login/lib/welcomepage.dart
@@ -8,31 +8,42 @@ class WelcomeView extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Container(
-            width: 76,
-            height: 76,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Color.fromRGBO(0, 0, 0, 0.25),
-                  offset: Offset(0, 4),
-                  blurRadius: 4,
-                ),
-              ],
-            ),
-            child: Icon(
-              Icons.done_all,
-              size: 50,
-              color: Colors.purple,
-            ),
-          ),
+          CircleDoneShape(),
           SizedBox(height: 50),
           SignInSuccessLabel(),
           SizedBox(height: 15),
           WelcomeLabel(),
         ],
+      ),
+    );
+  }
+}
+
+class CircleDoneShape extends StatelessWidget {
+  const CircleDoneShape({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 76,
+      height: 76,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Color.fromRGBO(0, 0, 0, 0.25),
+            offset: Offset(0, 4),
+            blurRadius: 4,
+          ),
+        ],
+      ),
+      child: Icon(
+        Icons.done_all,
+        size: 50,
+        color: Colors.purple,
       ),
     );
   }
